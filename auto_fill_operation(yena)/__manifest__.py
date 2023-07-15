ca7a87c23823f553ceb8917f5641777d3b868ba1
yenademir <116823062+yenademir@users.noreply.github.com>
2023-07-15 17:40:22 +0300

    Add files via upload

---
+++ auto_fill_operation(yena)/__manifest__.py
@@ -0,0 +1,28 @@
+{
+    'name': "auto_fill_operation(yena)",
+    'version': '1.0',
+    'summary': "Automatically fills certain fields upon sales.order and purchase.order",
+    'author': "Alperen Alihan ER",
+    'website': "https://yenaengineering.nl",
+    'category': 'Sales',
+    'license': 'LGPL-3',
+    'depends': ['base', 'sale_management', 'project', 'analytic', 'purchase'],
+    'data': [
+        'views/purchase_views/hiddenfields.xml',
+        'views/purchase_views/invisiblefields.xml',
+        'views/purchase_views/positioningfields.xml',
+        'views/purchase_views/requiredfields.xml',
+        'views/purchase_views/widgetchangefields.xml',
+        'views/sales_views/hiddenfields.xml',
+        'views/sales_views/invisiblefields.xml',
+        'views/sales_views/positioningfields.xml',
+        'views/sales_views/requiredfields.xml',
+        'views/sales_views/stringchangefields.xml',
+        'views/sales_views/widgetchangefields.xml'
+    ],
+    'demo': [
+        # 'demo/demo.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
