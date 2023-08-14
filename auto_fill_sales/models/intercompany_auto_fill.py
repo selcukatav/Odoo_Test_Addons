54ff82a0a4b063238e4f2110366abf9d519572cb
yenademir <116823062+yenademir@users.noreply.github.com>
2023-08-14 10:52:34 +0300

    Update intercompany_auto_fill.py

--- auto_fill_sales/models/intercompany_auto_fill.py
+++ auto_fill_sales/models/intercompany_auto_fill.py
@@ -11,9 +11,6 @@ class PurchaseOrder(models.Model):
         # Satın almadan satışa aktarılacak verileri alın
         purchase_order = self.env['purchase.order'].browse(self.id)
 
-        project = purchase_order.x_project_purchase
-        analytic_account_id = project.analytic_account_id.id if project.analytic_account_id else False
-
         # Şirket ve partner koşulları
         if purchase_order.company_id.id == 2 and purchase_order.partner_id.id == 1:
             # Satış siparişi değerleri
