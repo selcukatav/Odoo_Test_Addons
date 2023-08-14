edfa12f7754980f03c2d4dc25a894dd249941d38
yenademir <116823062+yenademir@users.noreply.github.com>
2023-08-14 11:56:07 +0300

    Update intercompany_auto_fill.py

--- auto_fill_sales/models/intercompany_auto_fill.py
+++ auto_fill_sales/models/intercompany_auto_fill.py
@@ -5,32 +5,36 @@ class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
     def _inter_company_create_sale_order(self, dest_company):
-        # Öncelikle orijinal metodun işlevselliğini çalıştır
-        sale_order = super()._inter_company_create_sale_order(dest_company)
-
-        # Satın almadan satışa aktarılacak verileri alın
+        # Özelleştirme kodunuzu burada ekleyebilirsiniz.
+        # Örneğin, satış siparişi değerlerini oluşturmak istiyorsanız:
         purchase_order = self.env['purchase.order'].browse(self.id)
-
         project = purchase_order.x_project_purchase
-        if project and hasattr(project, 'analytic_account_id') and project.analytic_account_id:
-            analytic_account_id = project.analytic_account_id.id
-        else:
-            analytic_account_id = False
+        analytic_account_id = project.analytic_account_id.id if project and hasattr(project, 'analytic_account_id') and project.analytic_account_id else False
 
         # Şirket ve partner koşulları
         if purchase_order.company_id.id == 2 and purchase_order.partner_id.id == 1:
-            sale_order.write({
+            # Satış siparişi değerleri
+            sale_order_vals = {
                 'x_project_sales': project.id if project else False,
                 'analytic_account_id': analytic_account_id,
-            })
+            }
 
-            # Satış siparişi satırlarını güncelle
-            for po_line, so_line in zip(purchase_order.order_line, sale_order.order_line):
-                so_line.write({
+            # Satın alma siparişi satırlarını döngüleyin
+            for po_line in purchase_order.order_line:
+                # Her bir satır için satış siparişi satırı oluştur
+                sale_line_vals = {
                     'product_id': po_line.product_id.id,
                     'name': po_line.name,
                     'product_uom_qty': po_line.product_qty,
                     'price_unit': po_line.price_unit,
-                })
+                }
+                sale_order_vals['order_line'].append((0, 0, sale_line_vals))
 
-        return sale_order
+            # Satış siparişini oluştur
+            sale_order = self.env['sale.order'].create(sale_order_vals)
+            # İlgili işlemler (onaylama, gönderme, vb.) burada yapılabilir
+
+        # Şimdi orijinal metodun işlevselliğini çağırın
+        res = super()._inter_company_create_sale_order(dest_company)
+
+        return res
