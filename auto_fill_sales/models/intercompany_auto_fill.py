11e70c119930fd1391d92a5895fb3ad3496dec59
yenademir <116823062+yenademir@users.noreply.github.com>
2023-08-14 11:44:53 +0300

    Update intercompany_auto_fill.py

--- auto_fill_sales/models/intercompany_auto_fill.py
+++ auto_fill_sales/models/intercompany_auto_fill.py
@@ -7,34 +7,30 @@ class PurchaseOrder(models.Model):
     def _inter_company_create_sale_order(self, dest_company):
         # Öncelikle orijinal metodun işlevselliğini çalıştır
         sale_order = super()._inter_company_create_sale_order(dest_company)
-        
-        return sale_order
-        
-        # Satış siparişi oluşturulmuşsa, özelleştirmeleri uygula
-        if sale_order:
-            # Satın almadan satışa aktarılacak verileri alın
-            purchase_order = self.env['purchase.order'].browse(self.id)
 
-            project = purchase_order.x_project_purchase
-            if project and hasattr(project, 'analytic_account_id') and project.analytic_account_id:
-                analytic_account_id = project.analytic_account_id.id
-            else:
-                analytic_account_id = False
+        # Satın almadan satışa aktarılacak verileri alın
+        purchase_order = self.env['purchase.order'].browse(self.id)
 
-            # Şirket ve partner koşulları
-            if purchase_order.company_id.id == 2 and purchase_order.partner_id.id == 1:
-                sale_order.write({
-                    'x_project_sales': project.id if project else False,
-                    'analytic_account_id': analytic_account_id,
+        project = purchase_order.x_project_purchase
+        if project and hasattr(project, 'analytic_account_id') and project.analytic_account_id:
+            analytic_account_id = project.analytic_account_id.id
+        else:
+            analytic_account_id = False
+
+        # Şirket ve partner koşulları
+        if purchase_order.company_id.id == 2 and purchase_order.partner_id.id == 1:
+            sale_order.write({
+                'x_project_sales': project.id if project else False,
+                'analytic_account_id': analytic_account_id,
+            })
+
+            # Satış siparişi satırlarını güncelle
+            for po_line, so_line in zip(purchase_order.order_line, sale_order.order_line):
+                so_line.write({
+                    'product_id': po_line.product_id.id,
+                    'name': po_line.name,
+                    'product_uom_qty': po_line.product_qty,
+                    'price_unit': po_line.price_unit,
                 })
 
-                # Satış siparişi satırlarını güncelle
-                for po_line, so_line in zip(purchase_order.order_line, sale_order.order_line):
-                    so_line.write({
-                        'product_id': po_line.product_id.id,
-                        'name': po_line.name,
-                        'product_uom_qty': po_line.product_qty,
-                        'price_unit': po_line.price_unit,
-                    })
-
-        
+        return sale_order
