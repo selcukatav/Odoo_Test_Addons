2fde6b357ea8c7c484552ba04a18be0b5e637357
yenademir <116823062+yenademir@users.noreply.github.com>
2023-08-14 09:16:29 +0300

    Add files via upload

---
+++ auto_fill_sales/models/intercompany_auto_fill.py
@@ -0,0 +1,41 @@
+from odoo import api, models
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+
+    @api.model
+    def _intercompany_create_sale(self):
+        # Satış siparişinin oluşturulması ve düzenlenmesi için metod
+        # Bu metodu uygun bir yere yerleştirin veya mevcut bir iş akışına entegre edin
+
+        # Satın almadan satışa aktarılacak verileri alın
+        purchase_order = self.env['purchase.order'].browse(self.id)
+
+        # Şirket ve partner koşulları
+        if purchase_order.company_id.id == 2 and purchase_order.partner_id.id == 1:
+            # Satış siparişi değerleri
+            sale_order_vals = {
+                'partner_id': purchase_order.partner_id.id,
+                'x_project_sales': purchase_order.x_project_purchase,
+                'analytic_account_id': purchase_order.analytic_account_id.id,
+                'order_line': []
+            }
+
+            # Satın alma siparişi satırlarını döngüleyin
+            for po_line in purchase_order.order_line:
+                # Her bir satır için satış siparişi satırı oluştur
+                sale_line_vals = {
+                    'product_id': po_line.product_id.id,
+                    'name': po_line.name,
+                    'product_uom_qty': po_line.product_qty,
+                    'price_unit': po_line.price_unit,
+                    'account_analytic_id': po_line.account_analytic_id.id,
+                }
+                sale_order_vals['order_line'].append((0, 0, sale_line_vals))
+
+            # Satış siparişini oluştur
+            sale_order = self.env['sale.order'].create(sale_order_vals)
+
+            # İlgili işlemler (onaylama, gönderme, vb.) burada yapılabilir
+
+            return sale_order
