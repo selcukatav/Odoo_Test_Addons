22882de6cf97a6179e403bc1ee6a0b374c280b2b
yenademir <116823062+yenademir@users.noreply.github.com>
2023-09-04 09:41:32 +0300

    Update mto_auto_fill.py

--- auto_fill_sales_model/models/mto_auto_fill.py
+++ auto_fill_sales_model/models/mto_auto_fill.py
@@ -26,7 +26,7 @@ class SaleOrder(models.Model):
                     # Satış siparişi satırını, ürün kimliği ile eşleştir
                     so_line = order.order_line.filtered(lambda line: line.product_id == po_line.product_id)
                     if so_line:
-                        new_price_unit = line.price_unit * 0.72  # Satış fiyatını 0.72 ile çarp
+                        new_price_unit = order_line.price_unit * 0.72  # Satış fiyatını 0.72 ile çarp
                         po_line.write({
                             'price_unit': new_price_unit,  # Yeni fiyatı güncelle
                             'account_analytic_id': order.analytic_account_id.id,
